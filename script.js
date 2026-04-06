205992b867d165894bf0c0dca7f5b0e06e1962be
Vitoria Silva <vitoriaconsyl@gmail.com>
2026-04-06 16:07:08 -0300

    colocando projetos no js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const menuToggle = document.getElementById("menu-toggle");
 const nav = document.querySelector("nav ul");
 const btnTopo = document.getElementById("btn-topo");
 
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
+menuToggle.addEventListener("click", () => { //Quando clicar no botao do menu hamburguer
+    nav.classList.toggle("active"); //adiciona ou remove a classe
 });
 
-document.addEventListener("click", (event) => {
+document.addEventListener("click", (event) => { //Detecta o clique em qualquer lugar da tela para fechar o menu
     if (!nav.contains(event.target) && !menuToggle.contains(event.target)) {
         nav.classList.remove("active");
     }
@@ -23,7 +23,7 @@ function validarFormulario(event) {
     const msg = document.querySelector("#msg").value.trim();
     const primeiroNome = nome.split(" ")[0];
 
-    if (nome === "" || email === "" || msg === "") {
+    if (nome === "" || email === "" || msg === "") { // Se algum campo estiver vazio
         alert("Por favor, preencha todos os campos antes de enviar.");
     } else {
         alert(`Obrigada, ${primeiroNome}! Sua mensagem foi enviada com sucesso.`);
@@ -33,7 +33,7 @@ function validarFormulario(event) {
 
 formulario.addEventListener(`submit`, validarFormulario);
 
-function atualizarTextoBotao() {
+function atualizarTextoBotao() { //Atualiza texto do botão
     if (body.classList.contains('dark')) {
         btnTema.textContent = "Claro";
     } else {
@@ -41,7 +41,7 @@ function atualizarTextoBotao() {
     }
 }
 
-function alternarTema() {
+function alternarTema() { //Alterna tema claro ou escuro
     body.classList.toggle('dark');
     atualizarTextoBotao();
 }
@@ -51,16 +51,51 @@ btnTema.addEventListener('click', alternarTema);
 atualizarTextoBotao();
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        btnTopo.style.display = "block";
+    if (window.scrollY > 300) { //Se rolou mais de 300px
+        btnTopo.style.display = "block"; //mostra o botão
     } else {
         btnTopo.style.display = "none";
     }
 });
 
-btnTopo.addEventListener("click", () => {
-    window.scrollTo({
+btnTopo.addEventListener("click", () => { // quando clicar no botão
+    window.scrollTo({ //rola suavemente até o topo
         top: 0,
         behavior: "smooth"
     });
 });
+
+//Array com os projetos
+
+const listaProjetos = [
+    {
+        titulo: "Portfólio (HTML, CSS e JavaScript)",
+        descricao: "Portfólio web desenvolvido com HTML, CSS e JavaScript, contendo layout responsivo, menu interativo e funcionalidades como validação de formulário e alternância de tema, com foco em usabilidade e organização das informações.",
+        link: "https://github.com/vitoriaconsyl-create/portfolio.git"
+    },
+    {
+        titulo: "Portfólio (React)",
+        descricao: "Aplicação web desenvolvida com React para apresentação profissional, contendo seções de projetos, informações pessoais e formulário de contato. O projeto utiliza componentização, responsividade e alternância de tema (claro/escuro), com foco em organização e experiência do usuário.",
+        link: "https://github.com/vitoriaconsyl-create/portfolio-react.git"
+    },
+    {
+        titulo: "Calculadora",
+        descricao: "Aplicação web desenvolvida com HTML, CSS e JavaScript para realização de operações matemáticas básicas. O projeto inclui validação de entradas, manipulação do DOM e interface responsiva, proporcionando uma experiência simples e funcional ao usuário.",
+        link: "https://github.com/vitoriaconsyl-create/calculadorasimples.git"
+    }
+];
+
+const container = document.getElementById("lista-projetos");
+if (container) {
+    listaProjetos.forEach((proj) => {
+        const card = document.createElement("article");
+        card.classList.add("cardProjeto");
+
+        card.innerHTML = `
+            <h3>${proj.titulo}</h3>
+            <p>${proj.descricao}</p>
+            <a href="${proj.link}" target="_blank">Ver projeto no GitHub</a>
+        `;
+        container.appendChild(card);
+    });
+}
